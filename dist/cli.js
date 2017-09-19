91039cb1a7aefd9137ded02520bd831385ff7315
craig-mulligan <craig@resin.io>
2017-09-19 14:59:35 +0300

    babel polyfill

--- dist/cli.js
+++ dist/cli.js
@@ -3,8 +3,8 @@
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-var capitano = require('capitano');
 var nextatic = require('./');
+var capitano = require('capitano');
 var pkg = require('../package.json');
 
 if (pkg.peerDependencies) {
